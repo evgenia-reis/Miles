f7b9f57848fcee3a71b8df959c0de79325bec0f6
Evgenia Reis <evgenia.kgz@gmail.com>
2023-02-27 22:57:28 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20_000;
+        int bonusMiles = price / 20;
+     System.out.println ("You have " + bonusMiles + " bonus miles");
+    }
+}
