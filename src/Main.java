9039946834ff89b275b60a8733d8b410ce59e025
Evgenia Reis <evgenia.kgz@gmail.com>
2023-02-27 23:16:03 +0600

    add rubAmount

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 20_000;
-        int bonusMiles = price / 20;
+        int price = 20000;
+        int rubAmount = 20;
+        int bonusMiles = price / rubAmount;
         System.out.println("You have " + bonusMiles + " bonus miles");
     }
 }
